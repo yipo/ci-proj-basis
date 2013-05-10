1165df945c1eac0e4d74f7044351c0c6b09d906f
YiPo <yipo.tw@hotmail.com>
2013-05-10 12:38:18 +0800

    Include the Bootstrap from BootstrapCDN.

--- ci-proj-admin/view.inc.php
+++ ci-proj-admin/view.inc.php
@@ -4,8 +4,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>ci-proj-admin</title>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css">
 </head>
 <body>
-	
+	<div class="navbar navbar-static-top">
+		<div class="navbar-inner">
+			<div class="container"><a href="#" class="brand"></a></div>
+		</div>
+	</div>
+	<div class="container"></div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
