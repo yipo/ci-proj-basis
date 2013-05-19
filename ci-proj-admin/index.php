b09ce751cf2b5049dce0ab7d3393bef7affd3561
YiPo <yipo.tw@hotmail.com>
2013-05-19 14:58:09 +0800

    Fix: the first time coming in.

--- ci-proj-admin/index.php
+++ ci-proj-admin/index.php
@@ -3,6 +3,15 @@
 require_once('model.inc.php');
 
 $model->get_ready();
+
+if (empty($_SERVER['HTTPS'])) {
+	if (!isset($_GET['fresh'])) {
+		exit(header('Refresh: 0; url=?fresh'));
+	} else {
+		exit('something went wrong! X-(');
+	}
+}
+
 $model->load();
 
 require_once('view.inc.php');
